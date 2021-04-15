4a13edfe5e872748cbe71ec66b2d14f4a124be3a
nurulfaidhah56 <nurulfaidhah1756@gmail.com>
2021-04-15 15:35:57 +0700

    commit module

---
+++ PBO Karyawan/PBO.py
@@ -0,0 +1,24 @@
+class karyawan:
+    '''Dasar kelas untuk semua karyawan'''
+    jumlah_karyawan = 0
+
+    def __init__(self, nama, gaji):
+        self.nama = nama 
+        self.gaji = gaji 
+        Karyawan.jumlah_karyawan += 1
+
+    def tampilkan_jumlah(self):
+        print("Total karyawan", karyawan.jumlah_karyawan)
+
+    def tampilkan_profil(self):
+        print("Nama :", self.nama)
+        print("Gaji :", self.gaji)
+
+#membuat objek pertama dari kelas karyawan 
+karyawan1 = Karyawan("Sarah", 1000000)
+#membuat objek kedua dari kelas karyawan
+karyawan2 = Karyawan("Budi", 2000000)
+
+karyawan1.tampilkan_profil()
+karyawan2.tampilkan_profil()
+print("Total karyawan :", karyawan.jumlah_karyawan)
